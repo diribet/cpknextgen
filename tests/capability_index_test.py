57bef15e257d217feffaf3b97f1755bdea363778
Stanislav Král <99830617+KralSt@users.noreply.github.com>
2024-04-22 17:15:14 +0200

    Round test values and add error checking tests
    Co-authored-by: Vlastimil Dolejš <vlastimil.dolejs@diribet.cz>

--- tests/capability_index_test.py
+++ tests/capability_index_test.py
@@ -18,29 +18,38 @@ def run_data_test(file_index, test_data):
 
     # Extract the required data for the test
     data = np.array(current_set["data"])
+    error = current_set["error"]
     upper_extreme = current_set["upper_extreme"]
     lower_extreme = current_set["lower_extreme"]
     upper_specification = current_set["upper_specification"]
     lower_specification = current_set["lower_specification"]
-    cp_pointed_expected = current_set["cp_point"]
-    cpk_pointed_expected = current_set["cpk_point"]
-    cp_interval = current_set["cp_interval"]
-    cpk_interval = current_set["cpk_interval"]
 
-    # Perform the evaluation on the dataset
-    result = evaluate(
-        np.array(data),
-        specification_limits=(lower_specification, upper_specification),
-        data_boundaries=(lower_extreme, upper_extreme),
-        random_seed=0
-    )
+    if error is not None:
+        with pytest.raises(Exception, match=error):
+            evaluate(data,
+                     specification_limits=(lower_specification, upper_specification),
+                     data_boundaries=(lower_extreme, upper_extreme),
+                     random_seed=0,
+                     samples=200)
 
-    # Assert the results
-    assert_almost_equal(result.capability_index.cp.point, cp_pointed_expected, decimal=2)
-    assert_almost_equal(result.capability_index.cpk.point, cpk_pointed_expected, decimal=2)
-    for i in range(2):
-        assert_almost_equal(result.capability_index.cp.interval[i], cp_interval[i], decimal=2)
-        assert_almost_equal(result.capability_index.cpk.interval[i], cpk_interval[i], decimal=2)
+    else:
+        cp_pointed_expected = current_set["cp_point"]
+        cpk_pointed_expected = current_set["cpk_point"]
+        cp_interval = current_set["cp_interval"]
+        cpk_interval = current_set["cpk_interval"]
+
+        # Perform the evaluation on the dataset
+        result = evaluate(np.array(data),
+                          specification_limits=(lower_specification, upper_specification),
+                          data_boundaries=(lower_extreme, upper_extreme),
+                          random_seed=0)
+
+        # Assert the results
+        assert_almost_equal(result.capability_index.cp.point, cp_pointed_expected, decimal=2)
+        assert_almost_equal(result.capability_index.cpk.point, cpk_pointed_expected, decimal=2)
+        for i in range(2):
+            assert_almost_equal(result.capability_index.cp.interval[i], cp_interval[i], decimal=2)
+            assert_almost_equal(result.capability_index.cpk.interval[i], cpk_interval[i], decimal=2)
 
 
 test_data = load_test_data()
@@ -73,4 +82,14 @@ def test_index_outliers(file_index):
 @pytest.mark.parametrize("file_index", [29])
 def test_index_heavy_tail(file_index):
     # Test the capability index calculation of different continuous data sets
+    run_data_test(file_index, test_data)
+
+
+@pytest.mark.parametrize("file_index", [8, 10, 178])
+def test_error_non_unique_values(file_index):
+    run_data_test(file_index, test_data)
+
+
+@pytest.mark.parametrize("file_index", [559, 560, 561])
+def test_error_not_enough_values(file_index):
     run_data_test(file_index, test_data)
